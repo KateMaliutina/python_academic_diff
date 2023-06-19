c14843ff8bdcf92f1c68cf56a173623e1fac9e3e
KateMaliutina <80636356+KateMaliutina@users.noreply.github.com>
2023-06-19 17:11:26 +0300

    5 labs done

--- homework05/vkapi/config.py
+++ homework05/vkapi/config.py
@@ -2,6 +2,8 @@
 
 VK_CONFIG = {
     "domain": "https://api.vk.com/method",
-    "access_token": "",
+    "access_token": "vk1.a.H3N0FvxVipkCPP6XFNj_XR4fzO9O45SJBArUf8dPF8Dknnhl141GQDUKpFEqhXqVkcX26KdYjgtSueSavA3Dr2jNLxjd0cuSjBjflJyaAah66Qit6c4aQvBD7wruoV8QZ2T3s_hse8r4hQn7l02o4xdlqevSn5UlDgwroGMeqVIDVhI9AKrKLJD0gARFiI",
     "version": "5.126",
+    "client_id": 51490491,
+    "target_limit": 100
 }
